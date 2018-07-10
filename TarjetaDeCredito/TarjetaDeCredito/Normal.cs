b25b66cf894c67b0348c221a9f326b1c584794d4
NicolasGabrielPerez <nico__1993@hotmail.com>
2018-07-10 19:19:34 -0300

    Tarjeta funcionando

---
+++ TarjetaDeCredito/TarjetaDeCredito/Normal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TarjetaDeCredito
+{
+    class Normal : State
+    {
+        public override Boolean tieneLimite(int monto)
+        {
+            return true;
+        }
+    }
+}
