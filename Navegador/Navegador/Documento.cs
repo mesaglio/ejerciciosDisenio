11dea5f6a635a8c31a9d046658507958b36b2098
juan <juan.mesaglio@gmail.com>
2018-07-10 22:11:14 -0300

    Navegatorrrr

---
+++ Navegador/Navegador/Documento.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Navegador
+{
+    class Documento
+    {
+        NavegadorWeb web;//= new NavegadorWeb();
+        Extension extension;
+        public Documento(NavegadorWeb _web, Extension _extension)
+        {
+            web = _web;
+            extension = _extension;
+        }
+        public void abrirEnWeb()
+        {
+
+            bool tengoLaExtencion = web.ExtensionesDisponibles().Contains(extension);
+            if (tengoLaExtencion)
+            {
+                extension.Abrir();
+            }
+            else { Console.WriteLine("Este documento tiene una extension desconocida"); }
+        }
+    }
+}
--- Navegador/Navegador/Program.cs
+++ Navegador/Navegador/Program.cs
@@ -28,38 +28,10 @@ namespace Navegador
         }
     }
 
-    class Documento
-    {
-        NavegadorWeb web;//= new NavegadorWeb();
-        Extension extension;
-        public Documento(NavegadorWeb _web, Extension _extension)
-        {
-            web = _web;
-            extension = _extension;
-        }
-        public void abrirEnWeb()
-        {
+    
 
-            bool tengoLaExtencion = web.ExtensionesDisponibles().Contains(extension);
-            if (tengoLaExtencion)
-            {
-                extension.Abrir();
-            }
-            else { Console.WriteLine("Este documento tiene una extension desconocida"); }
-        }
-    }
+    
 
-    abstract class Extension
-    {
-        abstract public void Abrir();
-    }
-
-    class Visio : Extension
-    {
-        public override void Abrir() { }
-    }
-    class Pdf : Extension
-    {
-        public override void Abrir() { }
-    }
+    
+    
 }
